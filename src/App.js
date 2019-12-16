49d0f71b00a9a528a4ef4cef961c5216c11bad8f
Timgill987 <56654688+Timgill987@users.noreply.github.com>
2019-12-16 16:08:32 -0500

    Finished MVP

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
+import {BrowserRouter as Route} from "react-router-dom";
+
+
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation/>
+    <Route exact path="/">
+          
+        </Route>
+        <Route path="/about">
+          
+        </Route>
+        <Route path="/contact">
+          
+        </Route>
+
   </div>
 );
 
